ad57e8ff93475c06a38ab4007f8508bd35cedc6b
Cawe541 <cawe.wewe04@gmail.com>
2023-10-21 15:28:32 -0300

    feat(cursors): add presence track and list

--- lib/realtime_cursor_tracking_web/live/cursors.ex
+++ lib/realtime_cursor_tracking_web/live/cursors.ex
@@ -1,20 +1,58 @@
 defmodule RealtimeCursorTrackingWeb.Cursors do
+  alias RealtimeCursorTrackingWeb.Presence
   use RealtimeCursorTrackingWeb, :live_view
+
+  @channel_topic "cursor_page"
 
   def mount(_params, _session, socket) do
     username = MnemonicSlugs.generate_slug()
 
-    {:ok,
-     socket
-     |> assign(:x, 50)
-     |> assign(:y, 50)
-     |> assign(:username, username)}
+    Presence.track(self(), @channel_topic, socket.id, %{
+      socket_id: socket.id,
+      x: 50,
+      y: 50,
+      username: username
+    })
+
+    RealtimeCursorTrackingWeb.Endpoint.subscribe(@channel_topic)
+
+    initial_users =
+      Presence.list(@channel_topic)
+      |> Enum.map(fn {_, data} -> data[:metas] |> List.first() end)
+
+    updated =
+      socket
+      |> assign(:username, username)
+      |> assign(:users, initial_users)
+      |> assign(:socket_id, socket.id)
+
+    {:ok, updated}
   end
 
   def handle_event("cursor-move", %{"mouse_x" => x, "mouse_y" => y}, socket) do
-    {:noreply,
-     socket
-     |> assign(:x, x)
-     |> assign(:y, y)}
+    key = socket.id
+    payload = %{x: x, y: y}
+
+    metas =
+      Presence.get_by_key(@channel_topic, key)[:metas]
+      |> List.first()
+      |> Map.merge(payload)
+
+    Presence.update(self(), @channel_topic, key, metas)
+
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: _payload}, socket) do
+    users =
+      Presence.list(@channel_topic)
+      |> Enum.map(fn {_, data} -> data[:metas] |> List.first() end)
+
+    updated =
+      socket
+      |> assign(users: users)
+      |> assign(socket_id: socket.id)
+
+    {:noreply, updated}
   end
 end
