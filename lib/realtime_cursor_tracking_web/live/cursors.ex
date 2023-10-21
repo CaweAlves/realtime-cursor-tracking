082034f7cca652d971d68f871c8f2e2c4e38352e
Cawe541 <cawe.wewe04@gmail.com>
2023-10-21 14:59:33 -0300

    feat(cursors): add handle event for cursor position

--- lib/realtime_cursor_tracking_web/live/cursors.ex
+++ lib/realtime_cursor_tracking_web/live/cursors.ex
@@ -3,9 +3,15 @@ defmodule RealtimeCursorTrackingWeb.Cursors do
 
   def mount(_params, _session, socket) do
     {:ok,
-      socket
-        |> assign(:x, 50)
-        |> assign(:y, 50)
-    }
+     socket
+     |> assign(:x, 50)
+     |> assign(:y, 50)}
+  end
+
+  def handle_event("cursor-move", %{"mouse_x" => x, "mouse_y" => y}, socket) do
+    {:noreply,
+     socket
+     |> assign(:x, x)
+     |> assign(:y, y)}
   end
 end
