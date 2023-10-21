28fcc934a429a9bfffb7e67171ab12b88b83c65a
Cawe541 <cawe.wewe04@gmail.com>
2023-10-21 15:13:19 -0300

    update (cursors): add username when assembling page

--- lib/realtime_cursor_tracking_web/live/cursors.ex
+++ lib/realtime_cursor_tracking_web/live/cursors.ex
@@ -2,10 +2,13 @@ defmodule RealtimeCursorTrackingWeb.Cursors do
   use RealtimeCursorTrackingWeb, :live_view
 
   def mount(_params, _session, socket) do
+    username = MnemonicSlugs.generate_slug()
+
     {:ok,
      socket
      |> assign(:x, 50)
-     |> assign(:y, 50)}
+     |> assign(:y, 50)
+     |> assign(:username, username)}
   end
 
   def handle_event("cursor-move", %{"mouse_x" => x, "mouse_y" => y}, socket) do
